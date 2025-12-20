80b6d04c21c9f9eab3d02b217023065240bba62c
StarDust130 <csyadav0513@gmail.com>
2025-12-20 22:03:02 +0530

    feat: implement LandingPage component with structured layout and sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
 
-const page = () => {
+import  Footer  from "@/components/elements/footer";
+import FAQSection from "@/components/landing/FAQ";
+import FeaturesSection from "@/components/landing/FeaturesSection";
+import HeroSection  from "@/components/landing/hero-section";
+import {  Navbar } from "@/components/landing/navbar";
+import PricingSection from "@/components/landing/pricing";
+import SecuritySection from "@/components/landing/SecuritySection";
+
+
+export default function LandingPage() {
   return (
-    <div>Home page</div>
-  )
+    <div className="min-h-screen bg-background text-foreground font-sans selection:bg-primary/20">
+      <Navbar />
+      <main className="flex-1">
+        <HeroSection />
+        <FeaturesSection />
+        <SecuritySection />
+        <PricingSection />
+        <FAQSection />
+        {/* You can add a Pricing Section here later */}
+      </main>
+      <Footer />
+    </div>
+  );
 }
-export default page

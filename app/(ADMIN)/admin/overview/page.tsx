bb18450af204db01dc31cb3b3fb45e19cdea8674
StarDust130 <csyadav0513@gmail.com>
2026-01-03 13:59:59 +0530

    feat: implement OverviewPage component for admin overview

---
+++ app/(ADMIN)/admin/overview/page.tsx
@@ -0,0 +1,7 @@
+import OverviewClient from "@/components/pages/admin/overview-client";
+
+export const dynamic = "force-dynamic";
+
+export default function OverviewPage() {
+  return <OverviewClient />;
+}

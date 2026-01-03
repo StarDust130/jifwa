9c722d4cb3e2750a557df8c13c08267b4f00246a
StarDust130 <csyadav0513@gmail.com>
2026-01-03 13:59:51 +0530

    feat: create LatestPage component to display latest client information

---
+++ app/(ADMIN)/admin/latest/page.tsx
@@ -0,0 +1,7 @@
+import LatestClient from "@/components/pages/admin/latest-client";
+
+export const dynamic = "force-dynamic";
+
+export default function LatestPage() {
+  return <LatestClient />;
+}
